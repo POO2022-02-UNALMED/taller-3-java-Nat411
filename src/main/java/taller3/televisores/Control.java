f7164886a2b369bf6e7f4f98b8026316698ab355
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2022-08-28 18:59:32 -0500

    Create Control.java

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,51 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	
+	
+	public void enlazar (TV tv) {
+		this.tv = tv;
+		tv.control = this;
+	}
+	
+	public void turnOn () {
+		this.tv.turnOn();
+	}
+	
+	public void turnOff () {
+		this.tv.turnOff();
+	}
+	
+	public void canalUp () {
+		this.tv.canalUp();
+	}
+	
+	public void canalDown () {
+		this.tv.canalDown();
+	}
+	
+	public void volumenUp () {
+		this.tv.volumenUp();
+	}
+	
+	public void volumenDown () {
+		this.tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		if ((canal >= 1 && canal <= 120) && this.tv.getEstado() == true) {
+			this.tv.setCanal(canal);
+		}
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	public TV getTv () {
+		return tv;
+	}
+	
+
+}
